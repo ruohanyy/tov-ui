bf92427e4b08eb626682609697b4ac4cf5e031b5
ruohanyy <2476245724@qq.com>
2024-12-10 19:41:20 +0800

    fix: fix import

--- packages/tov-ui/src/components.ts
+++ packages/tov-ui/src/components.ts
@@ -1,11 +1,11 @@
-export { default as Button } from './button'  
+export { default as Button } from './button'
 
-export { default as Input } from './Input'  
+export { default as Input } from './input'
 
-export { default as Tooltip } from './Tooltip'  
+export { default as Tooltip } from './tooltip'
 
-export { default as Table, TableColumn } from './Table'   
+export { default as Table, TableColumn } from './table'
 
-export { default as VirtualList } from './virtual-list' 
+export { default as VirtualList } from './virtual-list'
 
-export { default as Notification } from './notification' 
+export { default as Notification } from './notification'
